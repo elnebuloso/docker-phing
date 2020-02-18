cb7041310b8a8d93eb0226f8ea69d2562157bbc6
elnebuloso <jeff.tunessen@gmail.com>
2020-02-19 00:38:39 +0100

    removed phingcall from composer targets

--- main/src/Phing/Task/ComposerRunTask.php
+++ main/src/Phing/Task/ComposerRunTask.php
@@ -12,7 +12,12 @@ class ComposerRunTask extends AbstractTask
     /**
      * @var string
      */
-    private string $command;
+    private string $command = '';
+
+    /**
+     * @var string
+     */
+    private string $directory = '';
 
     /**
      * @param string $command
@@ -20,6 +25,14 @@ class ComposerRunTask extends AbstractTask
     public function setCommand(string $command): void
     {
         $this->command = $command;
+    }
+
+    /**
+     * @param string $directory
+     */
+    public function setDirectory(string $directory): void
+    {
+        $this->directory = $directory;
     }
 
     /**
@@ -31,16 +44,19 @@ class ComposerRunTask extends AbstractTask
 
         $target = $this->getProject()->getProperty('composer_runner') === 'docker' ? 'run:composer:docker' : 'run:composer:phing';
         $commands = array_filter(array_map('trim', explode(',', $this->getProject()->getProperty('composer_commands_before'))));
+        $directory = $this->directory === '' ? $this->getProject()->getProperty('project_dir_main_composer') : $this->directory;
 
         if ($target === 'run:composer:docker') {
             $this->getProject()->executeTarget('run:composer:docker:build');
         }
 
         foreach ($commands as $command) {
+            $this->getProject()->setProperty('run_composer_directory', $directory);
             $this->getProject()->setProperty('run_composer_command', $command);
             $this->getProject()->executeTarget($target);
         }
 
+        $this->getProject()->setProperty('run_composer_directory', $directory);
         $this->getProject()->setProperty('run_composer_command', $this->command);
         $this->getProject()->executeTarget($target);
 
